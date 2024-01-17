7e12f3f2c99e651e2b94292acc799b420258c4e1
unknown <lfernando.r991@gmail.com>
2024-01-17 15:51:49 -0300

    adding domain and infra enviroment-config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { PrismaModule } from './infrastructure/db/prisma.module'
+import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
-    })
+    }),
+    PrismaModule,
+    EnvironmentConfigModule
   ],
   controllers: [],
   providers: []
