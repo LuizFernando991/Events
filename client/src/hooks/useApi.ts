8f2876a2d775e3e3a55f1949b65ba0bb6be51611
unknown <lfernando.r991@gmail.com>
2024-01-19 09:39:35 -0300

    fixing refresh logic

--- client/src/hooks/useApi.ts
+++ client/src/hooks/useApi.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { useLocalStorage } from './useLocalStorage'
 import axios from 'axios'
 
@@ -7,21 +8,21 @@ export function useApi({
   shouldRefreshToken?: boolean
 }) {
   const { removeUserLocalStorage } = useLocalStorage()
-  const api = axios.create({
-    baseURL: import.meta.env.VITE_API_KEY,
-    withCredentials: true
-  })
+  const api = useMemo(() => {
+    return axios.create({
+      baseURL: import.meta.env.VITE_API_KEY,
+      withCredentials: true
+    })
+  }, [])
 
   if (shouldRefreshToken) {
     let isRefreshing = false
-    let refreshPromise: Promise<void> | null = null
 
     // Interceptor para capturar erros relacionados ao token
     api.interceptors.response.use(
       (response) => response,
       async (error) => {
         const originalRequest = error.config
-
         // Verifica se o erro é devido a um token expirado
         if (error?.response?.status && error.response.status === 401) {
           // Evita a execução simultânea de várias solicitações de atualização do token
@@ -29,40 +30,22 @@ export function useApi({
             isRefreshing = true
             try {
               console.log('refresh_token')
-              // Faça uma chamada para renovar o token
+              // Chamada para renovar o token
               const refreshResponse = await api.post('/auth/refresh')
               console.log(refreshResponse)
-              // Atualize o token no backend (se necessário)
 
               // Reenvie a solicitação original com o novo token do backend
               originalRequest.headers['Authorization'] =
                 `Bearer ${refreshResponse.data.token}`
               return api(originalRequest)
             } catch (refreshError) {
-              // Se houver um erro ao renovar o token, redirecione para a página de login ou faça algo apropriado
-              console.error('Erro ao renovar token:', refreshError)
+              // Se houver um erro ao renovar o token, deslogar usuário
               removeUserLocalStorage()
-              window.location.href = '/login'
               // Redirecionar para a página de login ou fazer algo apropriado
+              window.location.href = '/login'
             } finally {
               isRefreshing = false
             }
-          } else {
-            // Se já há uma solicitação de atualização do token em andamento, aguarde até que ela seja concluída
-            if (!refreshPromise) {
-              refreshPromise = new Promise((resolve) => {
-                const checkInterval = setInterval(() => {
-                  if (!isRefreshing) {
-                    clearInterval(checkInterval)
-                    resolve()
-                  }
-                }, 100)
-              })
-            }
-            await refreshPromise
-            refreshPromise = null
-            // Reenvie a solicitação original após a atualização do token
-            return api(originalRequest)
           }
         }
 
